a9592f659d5876a460da9cadc351de121629722e
Maxime <maxime.colin2@gmail.com>
2015-12-16 23:09:44 +0100

    Clean

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -8,17 +8,12 @@ import {
 
 // Load the implementations that should be tested
 import {App} from './app';
-import {Title} from './providers/title';
 
 describe('App', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEachProviders(() => [
-    App,
-    Title
-  ]);
+    App
+    ]);
 
-  it('should have a url', inject([ App ], (app) => {
-    expect(app.url).toEqual('https://twitter.com/AngularClass');
-  }));
 
 });
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,23 +2,20 @@ import {Component} from 'angular2/core';
 import {FORM_DIRECTIVES} from 'angular2/common';
 import {Http} from 'angular2/http';
 
-import {Title} from '../providers/title';
 
 @Component({
-  selector: 'home',
-  directives: [ FORM_DIRECTIVES ],
-  providers: [ Title ],
-  pipes: [],
-  styles: [ require('./home.css') ],
-  template: require('./home.html')
+	selector: 'home',
+	directives: [ FORM_DIRECTIVES ],
+	styles: [ require('./home.css') ],
+	template: require('./home.html')
 })
 export class Home {
-  // TypeScript public modifiers
-  constructor(public title: Title, public http: Http) {
-  }
 
-  ngOnInit() {
-    console.log('hello Home');
-  }
+	title:string;
+
+	constructor() {
+		this.title = 'boule';
+	}
+
 
 }
